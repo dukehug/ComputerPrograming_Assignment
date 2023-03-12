d0aa8e68cb846480df85b8d8d9784902e8443d38
Duke Hsu <116319266+dukehug@users.noreply.github.com>
2023-03-12 20:13:27 +0800

    Create Form1.cs

---
+++ LabLabrotaryActivity_3/Form1.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabLabrotaryActivity_3
+{
+
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void uiLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void uiButton1_Click(object sender, EventArgs e)
+        {
+
+            
+            string username, password;
+
+            username = textBoxUsername.Text;
+            password = textBoxPassword.Text;
+
+            if (username == "duke" && password == "123456")
+            {
+
+                MessageBox.Show("Welcome to computation of grades!");
+
+                Form2PagePerlimInput form2 = new Form2PagePerlimInput();
+                form2.Show();
+                this.Hide();
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("User and password incorrect! try again");
+                textBoxUsername.Clear();
+                textBoxPassword.Clear();
+                textBoxUsername.Focus();
+
+
+            }
+        }
+
+        private void rabtnClear_CheckedChanged(object sender, EventArgs e)
+        {
+            textBoxUsername.Clear();
+            textBoxPassword.Clear();
+            textBoxUsername.Focus();
+        }
+
+        private void labelClear_Click(object sender, EventArgs e)
+        {
+            textBoxUsername.Clear();
+            textBoxPassword.Clear();
+            textBoxUsername.Focus();
+        }
+    }
+}
