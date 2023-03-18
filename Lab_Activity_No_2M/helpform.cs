85f7b8f190083130787aff9839f3ca9bdc8039ab
Duke Hsu <116319266+dukehug@users.noreply.github.com>
2023-03-18 22:28:46 +0800

    Create helpform.cs

---
+++ Lab_Activity_No_2M/helpform.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_Activity_No_2M
+{
+    public partial class HelpForm : Form
+    {
+        public HelpForm()
+        {
+            InitializeComponent();
+            this.ControlBox = false;  //do not display control icons
+
+        }
+
+        private void uiTitlePanel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HelpForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
